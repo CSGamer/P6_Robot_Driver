634411a5201a6b0da2e467adec7f4f3fa73c8097
CSGamer <cs.schulz@live.dk>
2024-05-01 10:30:24 +0200

    update to cam_pack

---
+++ ROS2_Packages/build/camera_pack/CMakeFiles/cam_Nano.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cam_Nano.dir/src/rd_Nano_Ros.cpp.o"
+  "CMakeFiles/cam_Nano.dir/src/rd_Nano_Ros.cpp.o.d"
+  "cam_Nano"
+  "cam_Nano.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cam_Nano.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ROS2_Packages/build/camera_pack/CMakeFiles/cam_Nano.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cam_Nano.
+# This may be replaced when dependencies are built.
