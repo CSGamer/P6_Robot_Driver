b07ee75e24dccc4535c67db425fe5e2f05773cf2
Christoffer Schulz <43569465+CSGamer@users.noreply.github.com>
2024-05-15 12:02:06 +0200

    Test_10_Wheely

---
+++ ROS2_Packages/src/py_Driver/py_Driver/wheely.py
@@ -0,0 +1,86 @@
+import os
+import select
+import sys
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from rclpy.qos import QoSProfile
+
+
+class Controller(Node):
+    target_linear_velocity = 0.0
+    target_angular_velocity = 0.2
+    control_linear_velocity = 0.0
+    control_angular_velocity = 0.0
+    pub = None
+	
+    def __init__(self,publisher):
+        nano = 2
+        self.pub = publisher
+        self.get_logger().warning('controller started')
+
+
+class Program(Node):
+
+    def __init__(self,publisher):
+        self.get_logger().warning('starting controller ')    
+        contr = Controller(publisher)
+        self.get_logger().warning('starting main')
+        self.subscription = self.create_subscription(
+		String,
+			'Cam_Detections',
+			self.ang_sub,
+			10)
+        self.subscription  # prevent unused variable warning
+        self.subscription = self.create_subscription(
+		String,
+			'distance',
+			self.dist_sub,
+			10)
+        self.subscription  # prevent unused variable warning
+        self.get_logger().warning('main started')
+
+
+    def ang_sub(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.ang_reg(msg)
+
+    def ang_reg(self, msg):
+        self.drive_test()
+
+    def dist_sub(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.dist_reg(msg)
+
+    def dist_reg(self, msg):
+        self.drive_test()
+		
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    print("Starting Program")
+    qos = QoSProfile(depth=10)
+    node = rclpy.create_node('teleop_keyboard')
+    pub = node.create_publisher(Twist, 'cmd_vel', qos)
+
+	#node = rclpy.create_node('teleop_keyboard')
+    minimal_subscriber = Program(pub)
+
+    rclpy.spin(minimal_subscriber)
+
+	# Destroy the node explicitly
+	# (optional - otherwise it will be done automatically
+	# when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
