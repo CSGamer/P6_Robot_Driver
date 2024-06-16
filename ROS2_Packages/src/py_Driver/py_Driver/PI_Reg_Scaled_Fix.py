6e2192c0f0f28a91d170c9b33ad5d556ac6736a0
Christoffer Schulz <43569465+CSGamer@users.noreply.github.com>
2024-06-16 20:08:15 +0200

    Maybe Scaling Eliminated

---
+++ ROS2_Packages/src/py_Driver/py_Driver/PI_Reg_Scaled_Fix.py
@@ -0,0 +1,228 @@
+import os
+import select
+import sys
+import rclpy
+import math
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Twist
+from rclpy.qos import QoSProfile
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+LIN_VEL_STEP_SIZE = 0.01
+
+BURGER_MAX_LIN_VEL = 0.21
+BURGER_MAX_ANG_VEL = 2.84
+
+WAFFLE_MAX_LIN_VEL = 0.26
+WAFFLE_MAX_ANG_VEL = 1.82
+
+ANGLE_SET_POINT = 320 #img x-coordinate
+DIST_SET_POINT = 1500 #mm
+
+P_Ang = 1 #0.75
+D_Ang = 0 
+P_Dist = 10 #0.9
+I_Dist = 0 # 1 # 0.3
+
+
+class Controller(Node):
+    target_linear_velocity = 0.0
+    target_angular_velocity = 0.0
+    control_linear_velocity = 0.0
+    control_angular_velocity = 0.0
+    pub = None
+	
+    def __init__(self,publisher):
+        super().__init__('controller')
+        qos = QoSProfile(depth=10)
+        self.pub = publisher.create_publisher(Twist, 'cmd_vel', qos)
+        self.get_logger().info('controller started')
+
+    def make_simple_profile(self,output, input, slop):
+        if input > output:
+            output = min(input, output + slop)
+        elif input < output:
+            output = max(input, output - slop)
+        else:
+            output = input
+        return output
+
+    def constrain(self, input_vel, low_bound, high_bound):
+        if input_vel < low_bound:
+            input_vel = low_bound
+        elif input_vel > high_bound:
+            input_vel = high_bound
+        else:
+            input_vel = input_vel
+
+        return input_vel
+        
+    def check_linear_limit_velocity(self, velocity):
+        if TURTLEBOT3_MODEL == 'burger':
+            return self.constrain(velocity, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
+        else:
+            return self.constrain(velocity, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
+
+
+    def check_angular_limit_velocity(self, velocity):
+        if TURTLEBOT3_MODEL == 'burger':
+            return self.constrain(velocity, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
+        else:
+            return self.constrain(velocity, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
+
+    def set_angle(self, val):
+        #self.get_logger().warning('angle_val "%s"' % val)
+        val = val/54
+        #self.get_logger().error('motor_val "%s"' % val)
+        self.target_angular_velocity = self.check_angular_limit_velocity(val)
+
+    def set_velocity(self, val):
+        #self.get_logger().warning('speed_val "%s"' % val)
+        val = val/1204
+        #self.get_logger().error('motort_val "%s"' % val)
+        self.target_linear_velocity = self.check_linear_limit_velocity(val)
+
+    def drive(self):
+        twist = Twist()  
+        #self.control_linear_velocity = self.make_simple_profile(self.control_linear_velocity, self.target_linear_velocity, (LIN_VEL_STEP_SIZE / 2.0))
+        twist.linear.x = self.target_linear_velocity
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        #self.control_angular_velocity = self.make_simple_profile(self.control_angular_velocity, self.target_angular_velocity, (LIN_VEL_STEP_SIZE / 2.0))
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = self.target_angular_velocity
+        #self.get_logger().info('linear vel: "%s"  angular vel "%s"' %(twist.linear.x, twist.angular.z))
+        self.pub.publish(twist)        
+
+
+class Program(Node):
+    contr = None
+    int_err_dist = 0.0
+    def __init__(self,publisher):
+        super().__init__('program')
+        self.get_logger().info('starting controller ')    
+        self.contr = Controller(self)
+        self.get_logger().info('starting main')
+        self.subscription = self.create_subscription(
+		Float32,
+			'distance',
+			self.dist_sub,
+			10)
+        self.subscription  # prevent unused variable warning
+        self.subscription = self.create_subscription(
+		String,
+			'Cam_Detections',
+			self.ang_sub,
+			10)
+        self.subscription  # prevent unused variable warning
+        self.subscription = self.create_subscription(
+		String,
+			'looper',
+			self.looper,
+			10)
+        self.subscription  # prevent unused variable warning
+        self.get_logger().info('main started')
+
+    def looper(self,msg):
+        self.contr.drive()
+        
+    float_array = None
+
+    def ang_sub(self, msg):
+        #self.get_logger().info('I heard: "%s"' % msg.data)
+
+        #Structure [ID, x, y, width, height, FPS] 
+        # Remove brackets and split by comma
+        elements = msg.data.strip("[]\n").split(", ")
+        
+        cp = None
+        if self.float_array != None:
+            cp = self.float_array
+        
+        try: 
+            # Convert elements to floats
+            float_array = [float(element) for element in elements]
+            self.float_array = float_array
+        except:
+            self.float_array = cp
+            self.get_logger().warning('Could not convert element to float:')
+
+
+        error = None
+
+        if (self.float_array[0] == 1): 
+            error = ((self.float_array[1] + (self.float_array[3]/2)) - ANGLE_SET_POINT)/5.3
+            error = math.floor(error)
+            self.get_logger().info('Error Angle: %s' %(-error))
+            self.ang_reg(-error)
+            #print([flo for flo in float_array])
+        
+
+    def ang_reg(self, error):
+        p = P_Ang
+        #          degree rad             control size 2.84/260
+        error = (error*(math.pi/180)) * p * 0.0109
+        self.contr.set_angle(error)
+        #self.contr.drive()
+
+
+    def dist_sub(self, msg):
+        #self.get_logger().info('I heard: "%s"' % msg.data)
+        if (float(msg.data) != -100):
+            error =float(msg.data) -  DIST_SET_POINT
+            self.get_logger().info(f'goal dist: {DIST_SET_POINT} \t meas_dist: {float(msg.data)}')
+            if (error >= 0):
+                self.dist_reg(error)
+            else:
+                self.dist_reg(0)
+
+    def dist_reg(self, error):
+        #             m         control size 0.21/260
+        error = (error / 1000) * 0.000807
+        p = P_Dist
+        i = I_Dist
+        proportional_term = error * p
+        integral_term = self.int_err_dist * i
+        if error <= 0:
+            self.int_err_dist = 0
+        else: 
+            self.int_err_dist += error
+        control_signal = proportional_term + integral_term
+
+        self.contr.set_velocity(control_signal)
+        #self.contr.drive()
+
+    
+		
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    print("Starting Program")
+    #qos = QoSProfile(depth=10)
+    #node = rclpy.create_node('teleop_keyboard')
+    pub = None # node.create_publisher(Twist, 'cmd_vel', qos)
+
+	#node = rclpy.create_node('teleop_keyboard')
+    minimal_subscriber = Program(pub)
+
+    rclpy.spin(minimal_subscriber)
+
+	# Destroy the node explicitly
+	# (optional - otherwise it will be done automatically
+	# when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
